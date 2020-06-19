c1b83c71d653bd18f83a41b2aa7661f7b84fc8cb
ukcassohub <65535832+ukcassohub@users.noreply.github.com>
2020-06-19 20:39:25 +0900

    라우터 합치기 전
    라우터 합치기 전

--- homework/routes/joinUs.js
+++ homework/routes/joinUs.js
@@ -2,5 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get('/', function(req, res, next) {
+  if(req.session.userInfo){
+    return res.render('joinUs',{title:'성공', userInfo: req.session.userInfo});
+  }
+  res.render('article', { title: 'GREENPERSON' });//render는 한번만 실행되야해서
+  //리턴으로 한번만 되게한다.
+});
 
 module.exports = router;
